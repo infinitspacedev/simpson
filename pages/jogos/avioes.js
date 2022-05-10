3e2f188086a6169387fe7d936479bc336fe56904
kmaleao <udijonson@gmail.com>
2022-05-10 14:17:34 -0300

    jogotal

---
+++ pages/jogos/avioes.js
@@ -0,0 +1,10 @@
+export default function Desenho(){
+    return(
+        <>
+        <h2>avioes</h2>
+        
+        <iframe width="100%" height="500px" src="https://www.addictinggames.com/embed/html5-games/22886" scrolling="no"></iframe>
+        <button><a href='/'>sair</a></button>
+		</>
+    )
+}
---
+++ pages/jogos/jogotal.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function JOGOTAL(){
+    return(
+        <>
+        <h2>corona game</h2>
+        <iframe width='100%'height={540} src="https://previews.customer.envatousercontent.com/files/284246327/index.html" allowfullscreen></iframe>
+       <button><a href='/'>sair</a></button>
+        </>
+    )
+}
