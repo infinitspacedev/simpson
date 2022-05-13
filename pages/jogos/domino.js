3b0fe54bf0a2425aabdf187fca2de06bddb4dd1e
kmaleao <udijonson@gmail.com>
2022-05-13 16:27:29 -0300

    domino

---
+++ pages/jogos/domino.js
@@ -0,0 +1,14 @@
+export default function Domino(){
+    return(
+        <>
+        <h2>chadrex</h2>
+        
+        <iframe
+  width='100%'height={540}
+ src="https://playpager.com/embed/chess/index.html"
+ scrolling="no">
+</iframe>
+        <button><a href='/'>sair</a></button>
+		</>
+    )
+}
