bd8b6d9f127af8476e460484338771ab5bf5b15b
kmaleao <udijonson@gmail.com>
2022-05-14 18:15:00 -0300

    series

---
+++ pages/series/bojack.js
@@ -0,0 +1,23 @@
+export default function Bojack(){
+    return(
+        
+        <div>
+            <h1>Bojack horserman</h1><hr></hr>
+            <h2> temporada 01</h2><br></br>
+            <img src="/bojack.jpg"width={50}height={50}></img>
+            <a href="/BJKHRSMNT01EP01.mp4"download>bojack..episodio 01......baixe agora</a>
+            <br></br>
+            <br></br>
+            <img src="/bojack.jpg"width={50}height={50}></img>
+            <a href="/BJKHRSMNT01EP02.mp4"download>bojack..episodio 02......baixe agora</a>
+            <br></br>
+            <br></br>
+            <img src="/bojack.jpg"width={50}height={50}></img>
+            <a href="/BJKHRSMNT01EP03.mp4"download>bojack..episodio 03......baixe agora</a>
+            <br></br>
+            <br></br>
+            <button><a href='/'>sair</a></button>
+        </div>
+        
+    )
+}
